02be94d3104e67e22622936c40fdad8edf349029
hoangDe11111 <nguyenhoangde@gmail.com>
2024-01-10 20:00:52 +0700

    lab2

--- Lab2/js/lab2-5.js
+++ Lab2/js/lab2-5.js
@@ -17,3 +17,24 @@ fetch('https://65929f4fbb129707198fe18e.mockapi.io/tinhpv10/students')
   .catch(error => {
     console.error(error);
   });
+
+  fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+  .then(response => response.json())
+  .then(({ data }) => {
+    const dataBody = document.getElementById('dataNation');
+    const tableRows = data.map((item, index) => {
+      const { Nation, Year, Population } = item;
+      return `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${Nation}</td>
+          <td>${Year}</td>
+          <td>${Population}</td>
+        </tr>
+      `;
+    });
+    dataBody.innerHTML = [...tableRows].join('');
+  })
+  .catch(error => {
+    console.error(error);
+  });
